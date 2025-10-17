68251efd3cf91921153398af5e855ef25bbcf551
wantloli <1205u24@gmail.com>
2025-10-17 18:39:19 +0800

    17.4

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: import.meta.env.VITE_REACT_APP_BACKEND_BASEURL,
   headers: {
     "Content-Type": "application/json",
   },
